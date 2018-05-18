5915598f5cfc98c0cff420d07bceba1ba4071391
Sami Klein <sami.klein@lessannoyingcrm.com>
2018-05-18 10:12:42 -0500

    initial commit, 18th
    copying over static files

--- hello_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('config/init.php');
-
-//this won't work until you set up a database and connect to it with config.php
-
-$result = dbQuery("SELECT * FROM messages limit 1")->fetch();
-
-echo "<h1>".$result['MessageBody']."</h1>";
